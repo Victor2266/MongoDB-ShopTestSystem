03e86864d3d25ee2e75208c116a9f595eb708353
Victor2266 <46388269+Victor2266@users.noreply.github.com>
2024-11-18 17:59:26 -0500

    fixing add transaction issue and user array

--- loyalty-points-systemtest/src/models/Transaction.js
+++ loyalty-points-systemtest/src/models/Transaction.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const TransactionSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
   points: { type: Number, required: true },
-  type: { type: String, enum: ["purchase", "subscription", "event"], required: true },
+  type: { type: String, enum: ["purchase", "reward", "bonus", "subscription", "event"], required: true },
   createdAt: { type: Date, default: Date.now },
 });
 
